2f8d7d94329e245a359d75f13b45097d60a62d43
Brandon Liu <bliu20@dtechhs.org>
2018-04-10 20:20:58 -0700

    asdf

--- user.js
+++ user.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 var model = mongoose.model('user', new mongoose.Schema({
-	, Name: {type: String, unique: true}
-	, age: {type: String, unique: true}
-	, email: {type: String, unique: true}
+	Name: {type: String, unique: true}
+	, Age: {type: String, unique: true}
+	, Email: {type: String, unique: true}
 	, password: {type: String}
 }));
 
