14751d5bf4364c64ec4c34c2c8156e65a9719861
Brandon Liu <bliu20@dtechhs.org>
2018-04-10 19:41:47 -0700

    why

--- user.js
+++ user.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 
 var model = mongoose.model('user', new mongoose.Schema({
 	Name: {type: String, unique: true}
-	, email: {type: String, unique: true}
-	CheezQuestion: {type: String, unique: true}
-  likecheezit: {type: boolean}
-	Password: {type: String}
+	, Email: {type: String, unique: true}
+	, CheezQuestion: {type: String, unique: true}
+  , likecheezit: {type: boolean}
+	, Password: {type: String}
 }));
 
 exports.getModel = function() {
