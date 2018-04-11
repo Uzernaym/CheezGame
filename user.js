ea8e3a42f8185bf9d732aa52d9152d06f1eb08a6
Brandon Liu <bliu20@dtechhs.org>
2018-04-10 20:16:08 -0700

    ad;l

--- user.js
+++ user.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose');
 
 var model = mongoose.model('user', new mongoose.Schema({
-	Name: {type: String, unique: true}
-	, Email: {type: String, unique: true}
-	, CheezQuestion: {type: String, unique: true}
-  , likecheezit: {type: Boolean}
+	, Name: {type: String, unique: true}
+	, age: {type: String, unique: true}
+	, email: {type: String, unique: true}
 	, password: {type: String}
 }));
 
