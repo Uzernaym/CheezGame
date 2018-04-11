fbb1fee5fb49be78d616374488a8417634895ab2
Brandon Liu <bliu20@dtechhs.org>
2018-04-10 19:40:10 -0700

    oif

--- user.js
+++ user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var model = mongoose.model('user', new mongoose.Schema({
 	Name: {type: String, unique: true}
-	Email: {type: String, unique: true}
+	, email: {type: String, unique: true}
 	CheezQuestion: {type: String, unique: true}
   likecheezit: {type: boolean}
 	Password: {type: String}
