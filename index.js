e9137defafa2d6d2d6514a3f1fb5a5e5e69cc653
Brandon Liu <bliu20@dtechhs.org>
2018-04-14 14:53:37 -0700

    a;ldjf

--- index.js
+++ index.js
@@ -89,6 +89,15 @@ app.get('/home.css', (req, res, next) => {
 	res.sendFile(filePath);
 });
 
+app.get('/index.css', (req, res, next) => {
+
+	/* Get the absolute path of the html file */
+	var filePath = path.join(__dirname, './index.css')
+
+	/* Sends the html file back to the browser */
+	res.sendFile(filePath);
+});
+
 /* Defines what function to all when the server recieves any request from http://localhost:8080 */
 server.on('listening', () => {
 
