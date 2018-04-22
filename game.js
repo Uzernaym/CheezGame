d39ac9cb1252641190b65de7f8b0920ca49e357a
Brandon Liu <bliu20@dtechhs.org>
2018-04-21 17:47:39 -0700

    wtf

--- game.js
+++ game.js
@@ -225,11 +225,6 @@ $(function() {
 
   // Click events
 
-  // Focus input when clicking anywhere on login page
-  $loginPage.click(function () {
-    $currentInput.focus();
-  });
-
   // Focus input when clicking on the message input's border
   $inputMessage.click(function () {
     $inputMessage.focus();
