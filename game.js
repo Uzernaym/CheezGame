8baa8137afb2b0d7fdf992e5ed697ed2e26f8c10
Brandon Liu <bliu20@dtechhs.org>
2018-04-21 17:49:07 -0700

    wtf

--- game.js
+++ game.js
@@ -203,10 +203,6 @@ $(function() {
 	});
 
   $window.keydown(function (event) {
-    // Auto-focus the current input when a key is typed
-    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-      $currentInput.focus();
-    }
     // When the client hits ENTER on their keyboard
     if (event.which === 13) {
       if (username) {
