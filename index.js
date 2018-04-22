23609d9178de6143d2e66c8e05a1b8cfe345e180
Brandon Liu <bliu20@dtechhs.org>
2018-04-21 20:45:14 -0700

    wtf

--- index.js
+++ index.js
@@ -32,61 +32,61 @@ var dbAddress = process.env.MONGODB_URI || 'mongodb://127.0.0.1/cheezit';
 var numUsers = 0;
 
 io.on('connection', function (socket) {
-var addedUser = false;
+  var addedUser = false;
 
-// when the client emits 'new message', this listens and executes
-socket.on('new message', function (data) {
-	// we tell the client to execute 'new message'
-	socket.broadcast.emit('new message', {
-		username: socket.username,
-		message: data
-	});
-});
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
 
-// when the client emits 'add user', this listens and executes
-socket.on('add user', function (username) {
-	if (addedUser) return;
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
 
-	// we store the username in the socket session for this client
-	socket.username = username;
-	++numUsers;
-	addedUser = true;
-	socket.emit('login', {
-		numUsers: numUsers
-	});
-	// echo globally (all clients) that a person has connected
-	socket.broadcast.emit('user joined', {
-		username: socket.username,
-		numUsers: numUsers
-	});
-});
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
 
-// when the client emits 'typing', we broadcast it to others
-socket.on('typing', function () {
-	socket.broadcast.emit('typing', {
-		username: socket.username
-	});
-});
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
 
-// when the client emits 'stop typing', we broadcast it to others
-socket.on('stop typing', function () {
-	socket.broadcast.emit('stop typing', {
-		username: socket.username
-	});
-});
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
 
-// when the user disconnects.. perform this
-socket.on('disconnect', function () {
-	if (addedUser) {
-		--numUsers;
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
 
-		// echo globally that this client has left
-		socket.broadcast.emit('user left', {
-			username: socket.username,
-			numUsers: numUsers
-		});
-	}
-});
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
 
 
@@ -122,18 +122,6 @@ function startServer() {
 		});
 
 	});
-
-/*
-	app.post('/game', (req, res, next) => {
-
-		var username = req.body.userName;
-		var password = req.body.password;
-		setUsername(username, password, (err) => {
-			res.send({error: err});
-		});
-	});
-*/
-
 	app.get('/login', (req, res, next) => {
 		var filePath = path.join(__dirname, './login.html');
 		res.sendFile(filePath);
