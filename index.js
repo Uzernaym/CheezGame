7e357a10c3571df1d86a1ed09af904ad64558275
Brandon Liu <bliu20@dtechhs.org>
2018-05-01 20:57:04 -0700

    wtf

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function startServer() {
 
 	app.use(bodyParser.json({ limit: '16mb' }));
 	app.use(express.static(path.join(__dirname, 'public')));
-  app.use(session({secret: 'amandaisabootyhole'}));
+  app.use(session({secret: 'hola'}));
   app.use(passport.initialize());
   app.use(passport.session());
 
