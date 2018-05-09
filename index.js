2a59d6bbaa8da39260298bcb111523b36f4023ba
Brandon Liu <bliu20@dtechhs.org>
2018-05-08 19:55:46 -0700

    wtf

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function addSockets() {
 		});
 
 		socket.on('message', (message) => {
-			io.emit('newMessage', {user: user, message: $input.value});
+			io.emit('newMessage', {user: user, message});
 		});
 
 		/* RECIEVED A PLAYER UPDATE FROM A BROWSER */
