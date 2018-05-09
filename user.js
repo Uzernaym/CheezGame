687ba86b483e1d0066325fcf691d856e3be878e6
Brandon Liu <bliu20@dtechhs.org>
2018-05-08 21:56:36 -0700

    wtf

--- user.js
+++ user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var model = mongoose.model('user', new mongoose.Schema({
 	userName: {type: String, unique: true}
-	, email: {type: String, unique: true}
+	, email: {type: String}
 	, password: {type: String}
 	, salt: {type: String}
 	, avatar: {type: String}
