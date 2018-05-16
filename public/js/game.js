6f835424db30e4820079f86c7a06e765d02d750c
Brandon Liu <bliu20@dtechhs.org>
2018-05-15 21:57:05 -0700

    wtf

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,8 @@
 var gamePieces = {};
+var $canvas = document.querySelector('canvas');
 var context = $canvas.getContext('2d');
+$canvas.width = window.innerWidth;
+$canvas.height = window.innerHeight;
 
 //Scoring Variables
 var score = 1;
