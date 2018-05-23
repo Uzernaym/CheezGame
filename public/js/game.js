4283472ff446e0f9d1c4721e0d0822661ad783f1
Brandon Liu <bliu20@dtechhs.org>
2018-05-22 22:35:51 -0700

    wtf

--- public/js/game.js
+++ public/js/game.js
@@ -101,7 +101,7 @@ function drawBird() {
 function drawPlayers() {
 
 	var playerNames = Object.keys(gamePieces);
-	var pieceWidth = Math.min($canvas.width, $canvas.height) / 25;
+	var pieceWidth = Math.min($canvas.width, $canvas.height) / 10;
 	playerNames.forEach(function(playerName) {
 		var gamePiece = gamePieces[playerName];
 		if(!gamePiece.loaded) return;
