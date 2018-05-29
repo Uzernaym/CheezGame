9b69bc10526ac048e50785183186ecb15d287b64
Brandon Liu <bliu20@dtechhs.org>
2018-05-28 22:07:53 -0700

    wtf

--- public/js/game.js
+++ public/js/game.js
@@ -204,18 +204,18 @@ function randomX() {
     x = Math.floor(Math.random() * canvas.width);
     if (x < 30) {
         x = 30;
-    } else if (x + 30 > canvas.width) {
-        x = canvas.width - 30;
+    } else if (x + 30 > $canvas.width) {
+        x = $canvas.width - 30;
     }
     return x;
 }
 
 function randomY() {
-    y = Math.floor(Math.random() * canvas.height);
+    y = Math.floor(Math.random() * $canvas.height);
     if (y < 30) {
         y = 30;
-    } else if (y + 30 > canvas.height) {
-        y = canvas.height - 30;
+    } else if (y + 30 > $canvas.height) {
+        y = $canvas.height - 30;
     }
     return y;
 }
